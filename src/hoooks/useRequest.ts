9f9b2d282f357a138f2004fa14b288fe46d6ffa0
jacky <jacky123789456@gmail.com>
2024-08-03 19:33:40 +0800

    基本useRequest

--- src/hoooks/useRequest.ts
+++ src/hoooks/useRequest.ts
@@ -1,33 +1,47 @@
 import { useState } from "react";
 import useMount from "./useMount";
 
+interface IOptions {
+  params: Record<string, string>;
+  manual?: boolean;
+  onSuccess?: (res: unknown) => void;
+  onError?: (err: unknown) => void;
+}
+
 /**
  * 1. 實現元件初始化發送請求
  * 2. 手動觸發請求
  */
-const useRequest = (service: (params: Record<string, string>) => Promise<unknown>, params: Record<string, string>) => {
+const useRequest = (
+  service: (params: Record<string, string>) => Promise<unknown>,
+  options: IOptions,
+) => {
   const [ data, setData ] = useState<unknown>();
   const [ loading, setLoading ] = useState<boolean>(false);
 
   const init = (curParams: Record<string, string>) => {
     setLoading(true);
-    service(curParams).then((res) => {
+    return (service(curParams).then((res) => {
       setData(res);
       setLoading(false);
-    }).catch(() => {
+      options.onSuccess && options.onSuccess(res);
+    }).catch((err) => {
       setLoading(false);
-    })
+      options.onError && options.onError(err);
+    }))
   }
 
   useMount(() => {
-    init(params);
+    if (!options.manual) {
+      init(options.params);
+    }
   })
 
   const run = (runParams: Record<string, string>) => {
-    init(runParams)
+    return init(runParams);
   }
 
-  return [data, loading];
+  return { data, loading, run };
 };
 
 export default useRequest;
